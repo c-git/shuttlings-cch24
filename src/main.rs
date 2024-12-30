d771c0ad773d855b1a4f031dad273d84228e6327
One <43485962+c-git@users.noreply.github.com>
2024-12-30 12:35:43 -0500

    feat: capture span entry and exit

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,16 @@ use shuttle_actix_web::ShuttleActixWeb;
 
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    use tracing_subscriber::{
+        fmt::{self, format::FmtSpan},
+        prelude::*,
+        EnvFilter,
+    };
+
+    tracing_subscriber::registry()
+        .with(fmt::layer().with_span_events(FmtSpan::ACTIVE))
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")))
+        .init();
+
     Ok(shuttlings_cch24::modify_service_config.into())
 }
