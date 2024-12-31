e6dcae026ec612acdf9ad070c7d24e0544f40ce8
One <43485962+c-git@users.noreply.github.com>
2024-12-31 03:23:39 -0500

    Day 23 - Task 2

--- src/day23.rs
+++ src/day23.rs
@@ -1,9 +1,9 @@
-use actix_web::{web, HttpResponse};
+use actix_web::web;
 use tracing::instrument;
 
 #[instrument]
-async fn star() -> actix_web::Result<HttpResponse> {
-    todo!()
+async fn star() -> &'static str {
+    r#"<div id="star" class="lit"></div>"#
 }
 
 pub(crate) fn scope() -> actix_web::Scope {
