fbefe2118d442c2a49cab9e43abd4a0a68289af8
One <43485962+c-git@users.noreply.github.com>
2024-12-31 10:44:11 -0500

    Day 23 - Task 4

--- src/day23.rs
+++ src/day23.rs
@@ -10,6 +10,55 @@ enum Color {
     Purple,
 }
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum OrnamentState {
+    On,
+    Off,
+}
+
+impl FromStr for OrnamentState {
+    type Err = actix_web::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "on" => Self::On,
+            "off" => Self::Off,
+            other => return Err(error::ErrorImATeapot(format!("Unexpected state: {other}"))),
+        })
+    }
+}
+
+impl Display for OrnamentState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::On => "on",
+                Self::Off => "off",
+            }
+        )
+    }
+}
+
+impl OrnamentState {
+    fn toggle(&self) -> Self {
+        match self {
+            OrnamentState::On => Self::Off,
+            OrnamentState::Off => Self::On,
+        }
+    }
+
+    /// Needs to have a leading space for separation
+    fn as_class_str(&self) -> &'static str {
+        if self == &Self::On {
+            " on"
+        } else {
+            ""
+        }
+    }
+}
+
 impl FromStr for Color {
     type Err = actix_web::Error;
 
@@ -67,8 +116,20 @@ async fn present(path: web::Path<String>) -> actix_web::Result<String> {
     ))
 }
 
+#[instrument(ret, err(Debug))]
+async fn ornament(path: web::Path<(String, String)>) -> actix_web::Result<String> {
+    let (state, n) = path.into_inner();
+    let state: OrnamentState = state.parse()?;
+    let next_state = state.toggle();
+    Ok(format!(
+        r#"<div class="ornament{}" id="ornament{n}" hx-trigger="load delay:2s once" hx-get="/23/ornament/{next_state}/{n}" hx-swap="outerHTML"></div>"#,
+        state.as_class_str()
+    ))
+}
+
 pub(crate) fn scope() -> actix_web::Scope {
     web::scope("/23")
         .route("/star", web::get().to(star))
         .route("/present/{color}", web::get().to(present))
+        .route("/ornament/{state}/{n}", web::get().to(ornament))
 }
